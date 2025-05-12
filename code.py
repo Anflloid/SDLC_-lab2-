56ef62e852a89155934e4153e20cd238adb66c45
Андрій <zedyt56@gmail.com>
2025-05-12 19:08:51 +0300

    Додано код - (функіонал)

---
+++ code.py
@@ -0,0 +1,6 @@
+def add_numbers(a, b):
+    return a + b
+
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
+    print(f"Результат: {add_numbers(num1, num2)}")
